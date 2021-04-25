80a1303269b254d1633a370af3aed6bed0ac9065
Thibaud <thibaud.streignart@gmail.com>
2021-04-25 14:13:43 +0200

    creating the create community view

--- resources/views/community/create.blade.php
+++ resources/views/community/create.blade.php
@@ -1,24 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ 'Create a Community' }}</title>
+@section('title')
+    Create New Community
+@endsection
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+@section('content')
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @component('components/create-community')@endcomponent
+@endsection
 
-        <!-- Scripts -->
-        @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        
-        <p>Hey there, does this work?</p>
-    </body>
-</html>
